3acc7d9018ed8999e9ad5d96c0b952f756986962
Gerashenko Vladimir <gera8479@gmail.com>
2018-08-07 23:10:09 +0300

    refactor: request_trap crud (promise)

--- controllers/RequestTrapsController.js
+++ controllers/RequestTrapsController.js
@@ -1,4 +1,4 @@
-const { RequestTraps } = require('../models/RequestTraps');
+const {RequestTraps} = require('../models/RequestTraps');
 
 class RequestTrapsController {
     static helloAction(req, resp) {
@@ -6,45 +6,46 @@ class RequestTrapsController {
     }
 
     static requestTrapAction(req, resp) {
-        const requestUrlFirstPart = req.url.split('?')[0].split('/')[1];
-        const requestTrapUri = requestUrlFirstPart ? requestUrlFirstPart : '/';
+        const trap_id = req.url.split('?')[0].split('/')[1];
         const requestData = {
             method: req.method,
             requestUri: req.url,
             headers: req.headers,
-            queryParams: req.query,
-            body: req.body
+            queryParams: req.query
         };
-        RequestTraps.find({requestUri: requestTrapUri}, function (err, results) {
-            if (err) return console.error(err);
-            if (results.length) {
-                const request = results[0];
-                request.children.push(requestData);
-                request.save(function (err, request) {
-                    if (err) return console.error(err);
-                    resp.send(request)
-                })
-            } else {
-                const request = new RequestTraps({
-                    requestUri: requestTrapUri,
-                    children: [requestData]
+        RequestTraps
+            .findOne({requestUri: trap_id})
+            .then(result => {
+                if (result){
+                    result.children.push(requestData);
+                    return result.save()
+                } else {
+                    return new RequestTraps({
+                        requestUri: trap_id,
+                        children: [requestData]
+                    }).save();
+                }
+            })
+            .then(result => resp.send(result))
+            .catch(
+                error => {
+                    //only demonstration
+                    resp.status(500);
+                    resp.send(error)
                 });
-
-                request.save(function (err, request) {
-                        if (err) return console.error(err);
-                        resp.send(request)
-                    }
-                )
-            }
-        });
     }
 
     static requestsShowAction(req, resp) {
         const trap_id = req.params.trap_id;
-        RequestTraps.find({requestUri: trap_id}, function (err, results) {
-            if (err) return console.error(err);
-            resp.send(results);
-        });
+        RequestTraps.find({requestUri: trap_id})
+            .then(
+                results => resp.send(results),
+                error => {
+                    //only demonstration
+                    resp.status(500);
+                    resp.send(error)
+                }
+                );
     }
 }
 
